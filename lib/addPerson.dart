d264b40373930fcded517539def3187b3def8eea
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-03-20 22:23:11 +0300

    Add drawer

--- lib/addPerson.dart
+++ lib/addPerson.dart
@@ -10,11 +10,7 @@ class Formscreen extends StatefulWidget{
 }
 
 class FormscreenState extends State<Formscreen>{
-  String _name;
-  String _phonenumber;
   DateTime _selectedDate = DateTime.now();
-
-  final GlobalKey<FormState> _formKey =  GlobalKey<FormState>();
 
   Widget _buildDateFIeld(){
     return Container(
@@ -97,9 +93,33 @@ class FormscreenState extends State<Formscreen>{
           ),
         ),
       ),
-      // resizeToAvoidBottomPadding: false,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[ 
+                  Colors.teal, Colors.tealAccent
+                  ])
+              ),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      elevation: 10,
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      child: Image.asset('images/corona.JPG', width: 100, height: 100),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            CustomListTile('Add Person', Icons.add, addTapped()),
+            CustomListTile('View added People', Icons.people, listTapped()),
+          ],
+        ),
+      ),
       body: ListView(
-        // crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             child: Stack(
@@ -170,3 +190,57 @@ class FormscreenState extends State<Formscreen>{
   
 }
 
+addTapped(){
+
+}
+
+listTapped(){
+
+}
+
+class CustomListTile extends StatelessWidget{
+  final String _name;
+  final IconData _icon;
+  final Function onTap;
+
+  CustomListTile(this._name, this._icon, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(9, 0, 9, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey))
+        ),
+        child: InkWell(
+          splashColor: Colors.teal,
+          onTap: () => onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(_icon),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                      _name,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Montserrat'
+                      ),
+                    ),
+                  )
+                  ],
+                )
+              ],
+            ),
+          )
+        ),
+      )
+    );
+  }
+}
