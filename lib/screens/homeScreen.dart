409d272a604abedd776758ced35c0bb0f2c163d7
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-04-17 19:37:20 +0300

    improve homescreen ui

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -141,78 +141,222 @@ class HomeScreenState extends State<HomeScreen>{
           ),
         ),
       body: Container(
-        child: ListView(
+        child: Stack(
           children: <Widget>[
-            Text(
-              "Please read the following precautions",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.teal,
-                fontFamily: 'Montserrat'),
-            ),
             Container(
-              height: 60,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.green,
+                    Colors.teal,
+                  ]
+                )
+              ),
             ),
-            Image.asset(
-              'images/home.PNG',
-              width: 120,
-              height: 120,
-            ),
-            Text(
-              "Stay at home if there is no critical matter that needs your attention",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 15
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white60, width:  2.0)
+                        ),
+                        padding:  EdgeInsets.all(8),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Image.asset(
+                            "images/logo.png",
+                            height: 550,
+                            width: 550,
+                          )
+                        ),
+                      ),
+                    ]
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Genesis Technologies",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.white
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    "Please take the following precautions",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-              height: 40,
-            ),
-            Image.asset(
-              'images/wash.PNG',
-              width: 120,
-              height: 120,
-            ),
-            Text(
-              "Wash your hands frequently",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 15
+              margin: EdgeInsets.only(top: 300),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                )
               ),
             ),
             Container(
-              height: 40,
-            ),
-            Image.asset(
-              'images/shake.PNG',
-              width: 120,
-              height: 120,
-            ),
-            Text(
-              "Avoid hand shakes and keep your distance from people",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 15
+              margin: EdgeInsets.only(top: 200),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30)
+                )
               ),
-            ),
-            Container(
-              height: 40,
-            ),
-            Image.asset(
-              'images/eye.PNG',
-              width: 120,
-              height: 120,
-            ),
-            Text(
-              "Avoid touching your face",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 15
+              child: Padding(
+                padding: EdgeInsets.all(0),
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      width: 100,
+                      child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      elevation: 10,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/cough.PNG',
+                              height: 80,
+                            ),
+                            Text(
+                              "Cover your mouth and nose when yous sneeze or cough",
+                              style: TextStyle(
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      elevation: 10,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/eye.PNG',
+                              height: 80,
+                            ),
+                            Text(
+                              "Avoid touching your face with unwashed hands",
+                              style: TextStyle(
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      elevation: 10,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/home.PNG',
+                              height: 80,
+                            ),
+                            Text(
+                              "Stay home if you don't need to get out for critical matters",
+                              style: TextStyle(
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      elevation: 10,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/wash.PNG',
+                              height: 80,
+                            ),
+                            Text(
+                              "Wash your hands frequently for atleast 20 seconds with soap.",
+                              style: TextStyle(
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      elevation: 10,
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.asset(
+                              'images/shake.PNG',
+                              height: 80,
+                            ),
+                            Text(
+                              "Avoid hand shakes and keep your distance with people",
+                              style: TextStyle(
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ),
             ),
           ],
