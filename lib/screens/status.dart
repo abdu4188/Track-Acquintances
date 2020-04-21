84642fe3f5d982132746b42253eb0d3f70a93e54
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-04-21 11:23:58 +0300

    improve Stat page UI

--- lib/screens/status.dart
+++ lib/screens/status.dart
@@ -6,7 +6,9 @@ import 'package:sqflite/sqflite.dart';
 import 'package:track_aquintances/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:path/path.dart' as Path;
+import 'package:path/path.dart' as importedPath;
+import 'package:flutter_svg/flutter_svg.dart';
+import 'constant.dart';
 
 class StatusPage extends StatefulWidget{
   @override
@@ -16,7 +18,7 @@ class StatusPage extends StatefulWidget{
 }
 
 bool hasLoaded = false;
-String selectedValue = "";
+String selectedValue = "select country";
 int confirmed = 0;
 int death = 0;
 int recovered =0;
@@ -54,7 +56,7 @@ class StatusPageState extends State<StatusPage>{
 
   getDbData() async {
     var databasesPath = await getDatabasesPath();
-    String path = Path.join(databasesPath, "people.db");
+    String path = importedPath.join(databasesPath, "people.db");
 
     Database db;
     db = await openDatabase(path, version: 1);
@@ -148,136 +150,155 @@ class StatusPageState extends State<StatusPage>{
             ],
           ),
         ),
-      body: hasLoaded ? Center(
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: ListView(
-                shrinkWrap: true,
+      body: hasLoaded ? ListView(
+        children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 30, top: 50, right: 20),
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.green,
+                    Colors.teal
+                  ]
+                ),
+                image: DecorationImage(
+                  image: AssetImage("images/virus.png")
+                )
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start  ,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Center(
-                      child: Text(
-                        "Pick a country to view data",
-                        style: TextStyle(
-                          color: Colors.teal,
-                          fontFamily: "Montserrat",
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ),
+                  SizedBox(
+                    height: 20,
                   ),
-                  Container(
-                    width: 80,
-                      child: ButtonTheme(
-                        alignedDropdown: true,
-                        child: DropdownButton<String>(
-                          hint: Text(selectedValue),
-                          isExpanded: true,
-                          style: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 16,
-                            fontFamily: 'Montserrat'
-                          ),
-                          iconSize: 50,
-                          iconEnabledColor: Colors.teal,
-                          elevation: 10,
-                          items: stringCountries.map((String value){
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(), 
-                          onChanged: (String value) => {
-                            selectedValue = value,
-                            calculateValues(value),
-                          }
-                        ),
-                      ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
+                  Expanded(
+                    child: Stack(
                       children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "Confirmed: ",
-                              style: TextStyle(
-                                color: Colors.teal,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: "Montserrat",
-                                fontSize: 20
-                              ),
-                            ),
-                            Text(
-                              "$confirmed",
-                              style: TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 18
-                              ),
-                            )
-                          ],
+                        SvgPicture.asset(
+                          "assets/icons/Drcorona.svg",
+                          width: 265,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
                         ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "Deaths: ",
-                              style: TextStyle(
-                                color: Colors.teal,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: "Montserrat",
-                                fontSize: 20
-                              ),
-                            ),
-                            Text(
-                              "$death",
-                              style: TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 18
-                              ),
-                            )
-                          ],
+                        Positioned(
+                          top: 20,
+                          left: 140,
+                          child: Text(
+                            "Stay Home",
+                            style: kHeadingTextStyle.copyWith(color: Colors.white),
+                          ),
                         ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "Recovered: ",
-                              style: TextStyle(
-                                color: Colors.teal,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: "Montserrat",
-                                fontSize: 20
-                              ),
-                            ),
-                            Text(
-                              "$recovered",
-                              style: TextStyle(
-                                fontFamily: "Montserrat",
-                                fontSize: 18
-                              ),
-                            )
-                          ],
-                        ),
+                        Container()
                       ],
-                    )
+                    ),
                   )
                 ],
               ),
             ),
-            Text(
-              "Source: John Hopkins University",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.teal,
-                fontFamily: 'Montserrat'
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5)
               )
-          ]
-        )
-        ) :
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                    // value: selectedValue,
+                    hint: Text(selectedValue),
+                    items: stringCountries.map((String value){
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(), 
+                    onChanged: (String value) => {
+                      selectedValue = value,
+                      calculateValues(value),
+                    }
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        color: kShadowColor
+                      )
+                    ]
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Counter(
+                        color: kInfectedColor, 
+                        number: confirmed, 
+                        title: "Infected",
+                      ),
+                      Counter(
+                        color: kDeathColor, 
+                        number: death, 
+                        title: "Deaths",
+                      ),
+                      Counter(
+                        color: kRecovercolor, 
+                        number: recovered, 
+                        title: "Recovered",
+                      ),                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20, top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Source: John Hopkins University",
+                        style: kTitleTextStyle,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ) :
       Center(
         child: CircularProgressIndicator(          
         ),
@@ -336,6 +357,75 @@ show(){
       
     });
   }
+}
+
+
+class Counter extends StatelessWidget {
+  final int number;
+  final Color color;
+  final String title;
+  const Counter({
+    Key key, this.number, this.color, this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(0.26)
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(
+                color: color,
+                width: 2
+              )
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "$number",
+          style: TextStyle(
+            fontSize: 20,
+            color: color
+          ),
+        ),
+        Text(
+          title,
+          style: kSubTextStyle,
+        )
+      ],
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(size.width/2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+  
 }
 
 class CovidData{
